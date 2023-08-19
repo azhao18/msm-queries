ab56672cddb502a0ca0d9c3bd1dbb29586bcab8d
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-08-19 15:05:58 +0000

    debugged movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,7 @@ class MoviesController < ApplicationController
   def show
     the_id = params.fetch("the_id")
 
-    matching_records = Director.where({ :id => the_id})
+    matching_records = Movie.where({ :id => the_id})
 
     @the_movie = matching_records.at(0)
 
